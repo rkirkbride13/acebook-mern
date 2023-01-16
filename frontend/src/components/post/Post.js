ebefa1c54746d98a80916da1bbdc35407bdd5b98
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-16 15:00:51 +0000

    updated controller and post component to enable delete a post functionality

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,12 +1,51 @@
 import React from "react";
 import moment from "moment";
 
-const Post = ({ post }) => {
+const Post = ({ post, setPosts, token, setToken }) => {
   const dateTimeAgo = moment(new Date(post.createdAt)).fromNow();
+
+  // The delete the post with POST_ID
+  const handleClick = async () => {
+    const response = await fetch("/posts", {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Post_ID: post._id,
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      console.log("post NOT deleted");
+    } else {
+      console.log("post deleted");
+      window.localStorage.setItem("token", data.token);
+      setToken(window.localStorage.getItem("token"));
+
+      // State passed from feed used to update all posts on Feed.js
+      // This refreshes post list after deletion
+      if (token) {
+        fetch("/posts", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+          .then((response) => response.json())
+          .then(async (data) => {
+            window.localStorage.setItem("token", data.token);
+            setToken(window.localStorage.getItem("token"));
+            setPosts(data.posts);
+          });
+      }
+    }
+  };
+
   return (
     <article data-cy="post" key={post._id} className="post">
       <div>{post.message} </div>
       <div className="timestamp">{dateTimeAgo} </div>
+      <span onClick={handleClick}>delete</span>
     </article>
   );
 };
