32dfc760b2e0cc31386b2c1fb10c731a065b62a7
Matt Barber <mthbarber@gmail.com>
2023-01-17 12:19:31 +0000

    Add ability to toggle comments into view

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import moment from "moment";
 import PropTypes from "prop-types";
 import Comments from "../comment/Comments"
@@ -14,14 +14,24 @@ const Post = ({ post, token, setToken, post_id }) => {
     _id: PropTypes.string,
     message: PropTypes.string
   }
+
+  const [commentsView, setCommentsView] = useState(false)
+
+  const showComments = () => {
+    setCommentsView(!commentsView)    
+  }
+
   const dateTimeAgo = moment(new Date(post.createdAt)).fromNow();
   return (
     <article data-cy="post" key={post._id} className="post">
       <div>{post.message} </div>
       <div className="timestamp">{dateTimeAgo} </div>
-      <Comments token={token} setToken={setToken} post_id={post_id}/>
+      <button onClick={showComments}>Comments</button>
+      {commentsView && <div display="none"><Comments display={commentsView} token={token} setToken={setToken} post_id={post_id}/></div>}
     </article>
   );
 };
+
+
 
 export default Post;
